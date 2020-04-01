16e0ddabbb0b8fd58854c88dee868d020cec71c6
Bart Sakowski <bart.sakowski@gmail.com>
2020-04-01 02:24:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,6 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end
-  
-  def initialize(name, "")
-    @name = name
-    @breed = "Mutt"
   end
 end
